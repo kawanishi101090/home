59fd2b817ddc75291b157cf2a43676c4d9b333fa
kwns <r.kawanishi@ansurs.co.jp>
2024-05-13 23:39:32 +0900

    modified controller

--- src/main/java/com/rykwns/home/controller/MainController.java
+++ src/main/java/com/rykwns/home/controller/MainController.java
@@ -2,6 +2,8 @@ package com.rykwns.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -10,5 +12,12 @@ public class MainController {
     public String getMethodName() {
         return "index";
     }
+
+    @GetMapping("test1")
+    public String getMethodName(@RequestParam String param) {
+        return "test1";
+    }
+    
+    
     
 }
