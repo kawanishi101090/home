2d716b3678f2e2f512494c8650aa4cd5a6695ca5
kwns <r.kawanishi@ansurs.co.jp>
2024-05-23 23:50:38 +0900

    db接続

--- src/main/java/com/rykwns/home/controller/MainController.java
+++ src/main/java/com/rykwns/home/controller/MainController.java
@@ -1,21 +1,31 @@
 package com.rykwns.home.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.ui.Model;
 
 
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private JdbcTemplate JdbcTemplate;
+
     @GetMapping("index")
     public String getMethodName() {
         return "index";
     }
 
-    @GetMapping("test1")
-    public String getMethodName(@RequestParam String param) {
-        return "test1";
+    @GetMapping("test")
+    public String getMethodName(Model model) {
+        String sql = "SELECT MESSAGE FROM TEST";
+        String message = JdbcTemplate.queryForObject(sql, String.class);
+        model.addAttribute("message",message);
+        System.out.println(model);
+        return "test";
     }
     
     
